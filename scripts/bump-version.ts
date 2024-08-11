3466ae16d022a2c430de2d9cff106ecbb06146a6
kash / Hytus <kash@hytus.net>
2024-08-11 23:03:52 +0900

    feat(scripts): add bump-version.ts feat!: set node>=20 chore(deps): add tsx for deps
    Signed-off-by: kash / Hytus <kash@hytus.net>

---
+++ scripts/bump-version.ts
@@ -0,0 +1,55 @@
+import { readFile, readdir, writeFile } from "node:fs/promises"
+import path from "node:path"
+import { parseArgs, type ParseArgsConfig } from "node:util"
+
+import { version } from "../package.json" with { type: "json" }
+
+const workspaces = "./packages"
+const packagesJson = "package.json"
+const versionArray = version.split(".").map((v) => Number.parseInt(v))
+const [majorVersion, minorVersion, patchVersion] = versionArray
+
+const options: ParseArgsConfig["options"] = {
+  patch: {
+    type: "boolean",
+  },
+  minor: {
+    type: "boolean",
+  },
+  major: {
+    type: "boolean",
+  },
+}
+
+const {
+  values: { patch, minor, major },
+} = parseArgs({ options })
+
+let newVersion = ""
+
+if (major) {
+  newVersion = `${majorVersion + 1}.0.0`
+} else if (minor) {
+  newVersion = `${majorVersion}.${minorVersion + 1}.0`
+} else if (patch) {
+  newVersion = `${majorVersion}.${minorVersion}.${patchVersion + 1}`
+} else {
+  console.error("specify bump version")
+}
+
+const rootProjectPath = "./package.json"
+const rootProjectContent = await readFile(rootProjectPath, {
+  encoding: "utf8",
+})
+await writeFile(
+  rootProjectPath,
+  rootProjectContent.replace(version, newVersion)
+)
+const packages = await readdir(workspaces)
+await Promise.all(
+  packages.map(async (d) => {
+    const projectPath = path.join(workspaces, d, packagesJson)
+    const projectContent = await readFile(projectPath, { encoding: "utf8" })
+    await writeFile(projectPath, projectContent.replace(version, newVersion))
+  })
+)
